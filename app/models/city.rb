2b1f8e6395e26b4577dab9cd2d79c23ad910d901
Carolina Cota <carolinareycota@gmail.com>
2020-03-02 18:42:51 +0100

    Finalizing generating models.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,13 @@
 class City < ApplicationRecord
+  after_validation :set_slug, on: %i[create update]
+
+  def to_param
+    slug
+  end
+
+  private
+
+    def set_slug
+      self.slug = to_slug(name) if name.present?
+    end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,16 @@
 class Place < ApplicationRecord
+  after_validation :set_slug, on: %i[create update]
+
   belongs_to :city
   belongs_to :category
+
+  def to_param
+    slug
+  end
+
+  private
+
+    def set_slug
+      self.slug = to_slug(name) if name.present?
+    end
 end
