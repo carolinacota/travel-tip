e9e29aa7f9dc96b0b82275d3b46dfefac6e4574d
Virginia Garcia <virginia.a.garmar@gmail.com>
2020-03-03 14:18:51 +0100

    seeds for users and modification to List model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true
+  validates :bio, presence: true
 end
+
+User.destroy_all
+
+user1 = User.create(username: "firstuser", email: 'firstemail@gmail.com', bio: "I am the first amazing user", password: '123456')
+user2 = User.create(username: "seconduser", email: 'firstemail@gmail.com', bio: "I am the first amazing user", password: '123456')
+user3 = User.create(username: "thirduser", email: 'firstemail@gmail.com', bio: "I am the first amazing user", password: '123456')
+user4 = User.create(username: "fourthuser", email: 'firstemail@gmail.com', bio: "I am the first amazing user", password: '123456')
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_171846) do
+ActiveRecord::Schema.define(version: 2020_03_03_113852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_171846) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
+    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -53,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_171846) do
     t.decimal "longitude"
     t.bigint "city_id"
     t.bigint "category_id"
+    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_places_on_category_id"
@@ -73,13 +75,16 @@ ActiveRecord::Schema.define(version: 2020_03_02_171846) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "listplaces", "lists"
