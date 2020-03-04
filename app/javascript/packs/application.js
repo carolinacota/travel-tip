de544844a353852fd1774dbf31135bfec792dbd2
Carolina Cota <carolinareycota@gmail.com>
2020-03-04 16:06:31 +0100

    Get my current position not working to redirect to city page.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,6 @@ import "bootstrap";
 
 import { autocompleteSearch } from '../components/autocomplete'
 
+// import { getCurrentPosition } from '../plugins/geolocation'
+
 autocompleteSearch();
---
+++ app/javascript/plugins/geolocation.js
@@ -0,0 +1,20 @@
+// currentLocation = document.getElementById('current-location');
+
+// let getCurrentPosition = navigator.geolocation.getCurrentPosition((location) => {
+//   // console.log(location);
+//   console.log(location)
+//   let latitude = location.coords.latitude.toString();
+//   console.log(latitude);
+//   let longitude = location.coords.longitude.toString();
+//   let geocoder = Geocoder.search(latitude, longitude)
+//   console.log(geocoder.first.address);
+// });
+
+// currentLocation.addEventListener('click', (event) => {
+//   // // fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${accessToken}`)
+//   // //   .then(response => response.json())
+//   // //   .then((data) => {
+//   // //     console.log(data)
+//   //   // });
+// });
+
