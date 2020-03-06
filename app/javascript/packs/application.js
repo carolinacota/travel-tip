8d0f926052d84b59eb6145eb6c9fe1d816c3736e
Bill Jenkins <bjenkins@anewpt.com>
2020-03-06 15:53:25 +0100

    FEAT- map added to place show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,11 @@ import "bootstrap";
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
+import { placeMapbox } from '../plugins/place_mapbox';
 
 
 initMapbox();
+placeMapbox();
 
 import { autocompleteSearch } from '../components/autocomplete'
 
---
+++ app/javascript/plugins/place_mapbox.js
@@ -0,0 +1,66 @@
+import mapboxgl from 'mapbox-gl';
+
+const placeMapbox = () => {
+  const mapElement = document.getElementById('placeMap');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'placeMap',
+      style: 'mapbox://styles/mapbox/streets-v10'
+
+
+    });
+
+    $('#myModalPlace').on('shown.bs.modal', function() {
+        map.resize();
+      });
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+
+      const popup = new mapboxgl.Popup( { offset: 0 }).setHTML(marker.infoWindow);
+      new mapboxgl.Marker()
+
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+    });
+
+
+
+    if (markers.length === 0) {
+      map.setZoom(4)
+    } else if (markers.length === 1) {
+      map.setZoom(15);
+      map.setCenter([markers[0].lng, markers[0].lat])
+    } else {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach ((marker) => {
+        bounds.extend([marker.lng, marker.lat]);
+      });
+
+
+
+
+
+      map.fitBounds(bounds, { padding: 70, maxZoom: 14, duration: 3000 } )
+    }
+
+     map.addControl(
+        new mapboxgl.GeolocateControl({
+        positionOptions: {
+        enableHighAccuracy: true
+        },
+        fitBoundsOptions: {
+          maxZoom: 12
+        },
+        trackUserLocation: true
+        }),
+        'top-left'
+      );
+
+  }
+};
+
+export { placeMapbox };
