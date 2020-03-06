64b09aa4e46c7301a560411cee908f773b564d4a
Carolina Cota <carolinareycota@gmail.com>
2020-03-06 11:44:32 +0100

    Follow model created.+ Added has_many relationships to the User + methods that will help us following/unfollowing users and to check if a user is following another.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,11 @@ class User < ApplicationRecord
 
   has_one_attached :photo
 
-  # validates :bio, presence: true
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
 
   validates :username, presence: true, uniqueness: true,
             format: {
@@ -21,5 +25,18 @@ class User < ApplicationRecord
 
   def to_param
     username
+  end
+
+  def follow(user_id)
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
+  end
+
+  def following?(user_id)
+    relationship = Follow.find_by(follower_id: id, following_id: user_id)
+    return true if relationship
   end
 end
